68e816851adafc668cc2019353fc74ec06f632f0
dhyana1984 <xiongyi_1984@foxmail.com>
2018-11-23 18:54:50 +0800

    View

---
+++ Views/Views/Global.asax.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Views.Infrastructure;
+
+namespace Views
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            ViewEngines.Engines.Clear();//如果不加此句，则Razor引擎还会使用，加上后就只会用自定义引擎
+            ViewEngines.Engines.Add(new DebugDataViewEngine());
+        }
+    }
+}
---
+++ WorkingWithRazor/WorkingWithRazor/Infrastructure/CustomLocationViewEngine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WorkingWithRazor.Infrastructure
+{
+    //自建一个ViewEngine, 需要在Global.asax中注册
+    public class CustomLocationViewEngine: RazorViewEngine
+    {
+        public CustomLocationViewEngine()
+        {
+            ViewLocationFormats = new string[]
+            {
+                "~/Views/{1}/{0}.cshtml",
+                "~/Views/Common/{0}.cshtml"//将查找共享视图的位置修改为View/Common
+            };
+        }
+    }
+}
