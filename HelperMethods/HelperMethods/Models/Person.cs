86970c66dea16c08731fcd4095a8e923f294f735
dhyana1984 <xiongyi_1984@foxmail.com>
2018-11-28 16:53:54 +0800

    去掉一些重复注释

--- HelperMethods/HelperMethods/Models/Person.cs
+++ HelperMethods/HelperMethods/Models/Person.cs
@@ -12,25 +12,17 @@ namespace HelperMethods.Models
     [MetadataType(typeof(PersonMetadata))]//定义伙伴类
     public partial class Person
     {
-        //[HiddenInput(DisplayValue = false)]//用支架时渲染成一个隐藏的input, DisplayValue决定是否显示这个属性
-        //[ScaffoldColumn(false)]//是否使用基架
+
         public int PersonId { get; set; }
 
-        //[Display(Name ="First")] //修改LabelFor生成的显示名称
-        //[UIHint("MultilineText")]//指定该字段渲染的模板
         public string FirstName { get; set; }
-
-        //[Display(Name = "Last")]
+ 
         public string LastName { get; set; }
 
-        //[Display(Name = "Birth Date")]
-        //[DataType(DataType.Date)]//渲染这个字段成某个类型的input
         public DateTime BirthDate { get; set; }
-
 
         public Address HomeAddress { get; set; }
 
-        //[Display(Name = "Approved")]
         public bool IsApproved { get; set; }
         public Role Role { get; set; }
     }
