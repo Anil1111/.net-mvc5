cf56fc21f426b5dbfa91868525a0a5ccf4451011
dhyana1984 <xiongyi_1984@foxmail.com>
2018-12-03 14:04:55 +0800

    KockoutJS 替换原有form提交

--- WebService/WebService/Controllers/HomeController.cs
+++ WebService/WebService/Controllers/HomeController.cs
@@ -13,39 +13,41 @@ namespace WebService.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View(repo.GetAll());
+            //return View(repo.GetAll()); //不再传递模型，由客户端的Ajax请求WebAPI展现数据
+            return View();
         }
 
-        public ActionResult Add(Reservation item)
-        {
-            if (ModelState.IsValid)
-            {
-                repo.Add(item);
-                return RedirectToAction("Index");
-            }
-            else
-            {
-                return View("Index");
-            }
-        }
+        //已经用WebAPI替代这些CRUD方法
+        //public ActionResult Add(Reservation item)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        repo.Add(item);
+        //        return RedirectToAction("Index");
+        //    }
+        //    else
+        //    {
+        //        return View("Index");
+        //    }
+        //}
 
-        public ActionResult Remove(int id)
-        {
-            repo.Remove(id);
-            return RedirectToAction("Index");
-        }
+        //public ActionResult Remove(int id)
+        //{
+        //    repo.Remove(id);
+        //    return RedirectToAction("Index");
+        //}
 
-        public ActionResult Update(Reservation item)
-        {
-            if(ModelState.IsValid && repo.Update(item))
-            {
-                return RedirectToAction("Index");
-            }
-            else
-            {
-                return View("Index");
-            }
-        }
+        //public ActionResult Update(Reservation item)
+        //{
+        //    if(ModelState.IsValid && repo.Update(item))
+        //    {
+        //        return RedirectToAction("Index");
+        //    }
+        //    else
+        //    {
+        //        return View("Index");
+        //    }
+        //}
           
 
     }
